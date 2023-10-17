61f6027db1b434e1d9351db9ec3fc01b8f905902
Scruggerboy <lacrosseplayer96@gmail.com>
2023-10-17 18:37:09 -0400

    Add files via upload

---
+++ CFBPoll-master/README.md
@@ -0,0 +1,65 @@
+# CFBPoll 4.0 by TheAlpacalypse - The Pandas Rewrite
+
+Computerized poll to automatically rank college football teams each week
+First install the dependencies using the command:
+
+`pip install -r requirements.txt`
+
+Then run the program using the command:
+
+`python3 __main__.py`
+
+Use `Constants.py` to tweak the values I use to generate the ranking. I have tried to avoid leaving any raw values in this main program to let users experiment.
+
+---
+|Rank|Team|Flair|Record|Elo|Last Played|Result|Change|
+|---|---|---|---|---|---|---|---|
+| 1 | Clemson | [](#f/clemson) | (6 - 0) | 1815.71 | Syracuse | (**47** - 21) W | 7.53 |
+| 2 | Alabama | [](#f/alabama) | (5 - 0) | 1765.58 | Tennessee | (17 - **48**) W | 19.28 |
+| 3 | Notre Dame | [](#f/notredame) | (5 - 0) | 1721.9 | Pittsburgh | (3 - **45**) W | 29.03 |
+| 4 | Army | [](#f/army) | (6 - 1) | 1717.38 | Mercer | (**49** - 3) W | 54.41 |
+| 5 | Cincinnati | [](#f/cincinnati) | (4 - 0) | 1714.55 | SMU | (13 - **42**) W | 35.6 |
+| 6 | Appalachian State | [](#f/appalachianstate) | (3 - 1) | 1705.11 | Arkansas State | (**45** - 17) W | 20.94 |
+| 7 | Marshall | [](#f/marshall) | (5 - 0) | 1695.08 | Florida Atlantic | (**20** - 9) W | 18.22 |
+| 8 | BYU | [](#f/byu) | (6 - 0) | 1691.67 | Texas State | (**52** - 14) W | 11.37 |
+| 9 | Georgia | [](#f/georgia) | (3 - 1) | 1686.51 | Alabama | (41 - **24**) L | -24.35 |
+| 10 | Ohio State | [](#f/ohiostate) | (1 - 0) | 1685.59 | Nebraska | (**52** - 17) W | 17.99 |
+| 11 | LSU | [](#f/lsu) | (2 - 2) | 1683.88 | South Carolina | (**52** - 24) W | 19.67 |
+| 12 | Oklahoma | [](#f/oklahoma) | (3 - 2) | 1682.22 | TCU | (14 - **33**) W | 20.06 |
+| 13 | Liberty | [](#f/liberty) | (6 - 0) | 1663.18 | Southern Mississippi | (**56** - 35) W | 15.55 |
+| 14 | Florida | [](#f/florida) | (2 - 1) | 1658.25 | Texas A&M | (41 - **38**) L | -15.31 |
+| 15 | UCF | [](#f/ucf) | (3 - 2) | 1654.86 | Tulane | (**51** - 34) W | 18.43 |
+| 16 | SMU | [](#f/smu) | (5 - 1) | 1649.96 | Cincinnati | (**13** - 42) L | -35.6 |
+| 17 | Memphis | [](#f/memphis) | (3 - 1) | 1649.53 | Temple | (**41** - 29) W | 16.7 |
+| 18 | Louisiana | [](#f/louisiana) | (4 - 1) | 1641.8 | UAB | (20 - **24**) W | 15.97 |
+| 19 | Boise State | [](#f/nan) | (1 - 0) | 1637.54 | Utah State | (**42** - 13) W | 23.41 |
+| 20 | Oklahoma State | [](#f/oklahomastate) | (4 - 0) | 1631.04 | Iowa State | (**24** - 21) W | 10.87 |
+| 21 | Miami | [](#f/miami) | (5 - 1) | 1630.59 | Virginia | (**19** - 14) W | 11.39 |
+| 22 | North Carolina | [](#f/northcarolina) | (4 - 1) | 1628.21 | NC State | (**48** - 21) W | 24.03 |
+| 23 | Coastal Carolina | [](#f/coastalcarolina) | (5 - 0) | 1621.92 | Georgia Southern | (**28** - 14) W | 21.23 |
+| 24 | Michigan | [](#f/michigan) | (1 - 0) | 1621.08 | Minnesota | (24 - **49**) W | 33.49 |
+| 25 | Penn State | [](#f/pennstate) | (0 - 1) | 1620.48 | Indiana | (36 - **35**) L | -7.4 |
+|||||||||
+| 90 | Georgia Tech | [](#f/georgiatech) | (2 - 4) | 1437.02 | Boston College | (48 - **27**) L | -21.46 |
+|||||||||
+| 101 | Louisiana Monroe | [](#f/ulm) | (0 - 6) | 1354.71 | South Alabama | (38 - **14**) L | -22.47 |
+
+---
+
+**Mean Elo:** 1550.19
+
+**Median Elo:** 1545.54
+
+**Standard Deviation of Elo:** 93.96
+
+**Easiest Strength of Schedule:** Rice
+
+**Hardest Strength of Schedule:** Army
+
+**Predictions Quality (Season):** 60.93% Correct
+
+**Predictions Quality (Week):** 63.83% Correct (Last Week: 75.0%)
+
+[Explanation of the poll methodology here](https://www.reddit.com/user/TehAlpacalypse/comments/dwfsfi/cfb_poll_30_oops/)
+
+[Link to the github repository here](https://github.com/ChangedNameTo/CFBPoll)
---
+++ CFBPoll-master/__main__.py
@@ -0,0 +1,130 @@
+'pip install -r requirements.txt' #installed requirements.text file
+
+'pip install cfbd flatdict --quiet' # install cfbd and the flatdict library in Google Colab
+
+import cfbd
+import pandas as pd
+from matplotlib import pyplot as plt
+import flatdict
+
+#setting some configurations
+pd.set_option('display.max_columns', None) # do not truncate columns
+plt.style.use('ggplot')
+
+CFBD_API_KEY =('NzqjVKK0tMab5dMe8wXBGhql5o+jefFhsyjTA37Ad6QNrwaNOyMnqZYTy4VaF5go') # just paste in your API Key here instead of using dotenv and the os library
+
+config = cfbd.Configuration()
+config.api_key['Authorization'] = CFBD_API_KEY
+config.api_key_prefix['Authorization'] = 'Bearer'
+
+api = cfbd.StatsApi(cfbd.ApiClient(config))
+
+import pandas as pd
+import numpy as np
+import cfbd
+import plotly.express as px
+import os
+import math
+import datetime as dt
+import sys
+
+from alive_progress import alive_bar
+
+# Import my own functions
+sys.path.insert(1, './research')
+from Cycle import Cycle
+from stats_scraping import scrape_stats
+
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('mode.chained_assignment', None) # Disable this if you have issues. This is to supress a warning in the SOS section of the Team code that is working as intended. 
+
+#############
+# Constants #
+#############
+# Runtime Constants - Use these to change when or how the poll runs
+YEAR      = 2020
+WEEK      = 8
+RUN_SCRAPER = True # Run this if the values in the csvs are out of date
+
+# Value Constants - Tweak the poll outputs.
+# My default values are derived using the research contained in the /research directory.
+START_YEAR = 2010 
+TESTING = False # Set this to false if you care about poll output. 
+K_VALUE = 19
+HFA = 30 # Raw output is 3.04, converted to elo is 30.4. Rounds down to the whole number.
+FCS_ELO = 1204 # FCS Teams lose by, on average, 29.6 points. Converted to elo, 296. Average FBS team is 1500, so 1204 = 1500 - 296
+MEAN_REVERSION = 0.446153846153846 
+
+# Dependent Variable
+D_V = 'k_value'
+LOW_D_V = 16
+HIGH_D_V = 24
+STEPS = 11
+
+class Execution:
+    def build_options(self, iter_d_v=None):
+        self.options = {
+            'start_year':START_YEAR,
+            'testing':TESTING,
+            'k_value':K_VALUE,
+            'hfa':HFA,
+            'fcs_elo':FCS_ELO,
+            'mean_reversion':MEAN_REVERSION,
+            'end_year':YEAR,
+            'week':WEEK
+        }
+
+        if TESTING:
+            self.options[D_V] = iter_d_v
+
+    def run(self):
+        # Should we run the scraper? This is controlled in Constants
+        if RUN_SCRAPER:
+            scrape_stats(WEEK)
+
+        # Is this a testing run or a real run? Testing runs are significantly longer
+        if TESTING:
+            # Set up the progressbar
+            with alive_bar((STEPS * (YEAR + 1 - START_YEAR))) as bar:
+                # Set up the output dataframe
+                outputs = pd.DataFrame(columns=['iter_year','perc_correct','dv_value'])
+                
+                for iter_d_v in np.linspace(LOW_D_V, HIGH_D_V, STEPS):
+                    testing_games = pd.DataFrame()
+                    testing_teams = pd.DataFrame()
+                    testing_plays = pd.DataFrame()
+                    testing_stats = pd.DataFrame()
+
+                    for iter_year in range(START_YEAR, YEAR + 1):
+                        self.build_options(iter_d_v)
+
+                        bar()
+
+                        # Run the cycle
+                        polling_cycle = Cycle(self.options, iter_year)
+                        results = polling_cycle.run()
+
+                        # Append the iterated dependent variable to our data set
+                        results.append(iter_d_v)
+                        results_series = pd.Series(results, index=outputs.columns)
+                        outputs = outputs.append(results_series, ignore_index=True)
+
+                # Dump the DV outputs to a CSV
+                outputs.to_csv('research/D_V.csv')            
+
+                # Print out the DV graph
+                fig = px.scatter(outputs, x='dv_value',y='perc_correct')
+                fig.write_html('research/dv_test.html')
+        else:
+            with alive_bar(length=(YEAR + 1 - START_YEAR)) as bar:
+                for iter_year in range(START_YEAR, YEAR + 1):
+                    self.build_options()
+
+                    bar()
+                    polling_cycle = Cycle(self.options, iter_year)
+                    output = polling_cycle.run()
+
+execution = Execution()
+execution.run()
